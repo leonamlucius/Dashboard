2d1a9c6428f29276d708fc0ef12f9bab2ca2fe4e
leonamlucius <leonam253@gmail.com>
2025-04-18 20:39:56 -0300

    Mini dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -62,7 +62,7 @@ export function SidebarItem({ icon, text, active, alert }) {
       className={`
       relative flex items-center py-2 px-3 my-1
       font-medium rounded-md cursor-pointer
-      trasintions-colors
+      trasintions-colors group
       ${
         active
           ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
@@ -86,6 +86,19 @@ export function SidebarItem({ icon, text, active, alert }) {
             expanded ? "" : "top-2"
           }`}
         />
+      )}
+      {!expanded && (
+        <div
+          className={`
+        absolute left-full rounded-md px-2 py-1 ml-6
+        bg-indigo-100 text-indigo-800 text-sm
+        invisible opacity-20 -translate-x-3 transition-all
+        group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+      
+      `}
+        >
+          {text}
+        </div>
       )}
     </li>
   );
