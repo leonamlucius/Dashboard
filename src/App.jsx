8c73de435c6c96d3370d24f17cfdc1ca1a0b5596
leonamlucius <leonam253@gmail.com>
2025-04-18 00:05:35 -0300

    Sidebar working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
-import Sidebar from "./components/Sidebar";
+import Sidebar, { SidebarItem } from "./components/Sidebar";
+import {
+  LifeBuoy,
+  Receipt,
+  Boxes,
+  Package,
+  UserCircle,
+  BarChart3,
+  LayoutDashboard,
+  Settings,
+} from "lucide-react";
 
 function App() {
-
   return (
     <div className="bg-slate-500 w-screen h-screen flex ">
-      <Sidebar></Sidebar>
+      <Sidebar>
+        <SidebarItem
+          icon={<LayoutDashboard size={20} />}
+          text="Dashboard"
+          alert
+        />
+
+        <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" active />
+        <SidebarItem icon={<UserCircle size={20}/>} text="Users" />
+        <SidebarItem icon={<Boxes size={20}/>} text="Iventory" />
+        <SidebarItem icon={<Package size={20}/>} text="Orders" alert/>
+        <SidebarItem icon={<Receipt size={20}/>} text="Billings" />
+        <hr className="my-3"/>
+        <SidebarItem icon={<Settings size={20}/>} text="Settings"/>
+        <SidebarItem icon={<LifeBuoy size={20}/>} text="Help" />
+      </Sidebar>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,31 @@
-import { ChevronFirst, MoreVertical } from "lucide-react";
+import { ChevronFirst, ChevronLast, MoreVertical } from "lucide-react";
+import { createContext, useContext, useState } from "react";
 
+const SidebarContext = createContext();
 export default function Sidebar({ children }) {
+  const [expanded, setExpanded] = useState(true);
   return (
     <aside className="h-screen">
       <nav className="h-full flex flex-col bg-white shadow-sm rounded-md">
         <div className="p-4 pb-2 flex justify-between items-center">
-          <img src="images/logo.png" className="w-10" />
+          <img
+            src="images/logo.png"
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-11" : "w-0"
+            }`}
+          />
 
-          <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-            <ChevronFirst />
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+          >
+            {expanded ? <ChevronFirst /> : <ChevronLast />}
           </button>
         </div>
 
-        <ul className="flex-1 px-3">{children}</ul>
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
 
         <div className="border-t-1 flex p-3">
           <img
@@ -24,7 +37,8 @@ export default function Sidebar({ children }) {
           <div
             className={`
             flex justify-between items-center
-            w-52 ml-3
+           
+            overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
           
 
             `}
@@ -38,5 +52,41 @@ export default function Sidebar({ children }) {
         </div>
       </nav>
     </aside>
+  );
+}
+
+export function SidebarItem({ icon, text, active, alert }) {
+  const { expanded } = useContext(SidebarContext);
+  return (
+    <li
+      className={`
+      relative flex items-center py-2 px-3 my-1
+      font-medium rounded-md cursor-pointer
+      trasintions-colors
+      ${
+        active
+          ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+          : "hover:bg-indigo-50 text-gray-600"
+      }
+      
+    `}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all
+        ${expanded ? "w-52 ml-3" : "w-0"}
+        
+        `}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+            expanded ? "" : "top-2"
+          }`}
+        />
+      )}
+    </li>
   );
 }
